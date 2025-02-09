613ce58170dca6fa2be73c45394eb58df6cb6bab
Alexander Ricky <146740660+Alerica@users.noreply.github.com>
2025-02-09 17:40:58 +0700

    Player Animation + Walking and Running
    Adding Idle
    Adding Walking
    Adding Running
    Adding player flip

--- Assets/Scripts/PlayerContoller.cs
+++ Assets/Scripts/PlayerContoller.cs
@@ -1,3 +1,6 @@
+using System;
+using NUnit.Framework;
+using Unity.VisualScripting;
 using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -5,14 +8,75 @@ using UnityEngine.Pool;
 
 public class PlayerContoller : MonoBehaviour
 {
-    public float walkSpeed;
-    Vector2 moveInput;
-    public bool isMoving { get; private set; }
+    public float walkSpeed = 5f;
+    public float runSpeed = 8f;
 
+    public float CurrentMovementSpeeed { get 
+        {
+            if(IsMoving)
+            {
+                if(IsRunning) 
+                {
+                    return runSpeed;
+                } 
+                else 
+                {
+                    return walkSpeed;
+                } 
+            } 
+            else 
+            {
+                return 0;
+            }
+        }
+    }
+    Vector2 moveInput;
+    private bool isMoving = false;
+    private bool isRunning = false;
+
+    public bool isFacingRight = true;
+
+    public bool IsMoving { get 
+        {
+            return isMoving;
+        } 
+        private set 
+        {
+            isMoving = value;
+            animator.SetBool("IsMoving", IsMoving);
+        }
+    }
+
+    public bool IsRunning { get 
+        {
+             return isRunning;
+        }
+        private set
+        {
+            isRunning = value;
+            animator.SetBool("IsRunning", IsRunning);
+        }   
+    }
+
+    public bool IsFacingRight { get 
+        { 
+           return isFacingRight;
+        } 
+        private set 
+        {
+             if(isFacingRight != value) 
+             {
+                transform.localScale *= new Vector2(-1, 1);
+             }
+             isFacingRight = value;
+        }
+    }
     public Rigidbody2D rb2d;
+    Animator animator;
     void Awake() 
     {
         rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
     
     void Start()
@@ -28,7 +92,7 @@ public class PlayerContoller : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb2d.linearVelocity = new Vector2(moveInput.x * walkSpeed * Time.deltaTime, rb2d.linearVelocity.y);
+        rb2d.linearVelocity = new Vector2(moveInput.x * CurrentMovementSpeeed, rb2d.linearVelocity.y);
 
     }
 
@@ -40,7 +104,30 @@ public class PlayerContoller : MonoBehaviour
     public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
-        isMoving = moveInput != Vector2.zero;
+        IsMoving = moveInput != Vector2.zero;
+        SetFacingDirection(moveInput);
+    }
 
+    private void SetFacingDirection(Vector2 moveInput)
+    {
+        if(moveInput.x > 0 && !IsFacingRight)
+        {
+            IsFacingRight = true;
+        } 
+        else if(moveInput.x < 0 && IsFacingRight)
+        {
+            IsFacingRight = false;
+        }
+    }
+
+    public void OnRun(InputAction.CallbackContext context)
+    {
+        if(context.started)
+        {
+            IsRunning = true;
+        } else if(context.canceled) 
+        {
+            IsRunning = false;
+        }
     }
 }
